bbfa2f78ebfdd7d791525c4c42dd951c06ac28a3
SilverDragon <73411104+kagangtuya-star@users.noreply.github.com>
2025-08-08 15:53:47 +0800

    Update module.js

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,8 @@
-Hooks.once('init', async function() {
-
-});
-
 Hooks.once('ready', async function() {
-
+  Object.defineProperty(foundry.applications.apps.FilePicker.prototype, "canCreateFolder", {
+    get: function() { return true; }
+  });
+  Object.defineProperty(foundry.applications.apps.FilePicker.prototype, "canUpload", {
+    get: function() { return true; }
+  });
 });
